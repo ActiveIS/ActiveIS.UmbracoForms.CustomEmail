1fa4ae0548744f9e53e764315e896ff609960cd9
Aaron <aaron.sadler@activeis.net>
2020-05-28 14:06:48 +0100

    Registered smtpservice

---
+++ ActiveIS.UmbracoForms.CustomEmail/Composers/RegisterSmtpServiceComposer.cs
@@ -0,0 +1,15 @@
+﻿using ActiveIS.UmbracoForms.CustomEmail.Interfaces;
+using ActiveIS.UmbracoForms.CustomEmail.Services;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace ActiveIS.UmbracoForms.CustomEmail.Composers
+{
+    public class RegisterSmtpServiceComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Register<ISmtpService, SmtpService>(Lifetime.Request);
+        }
+    }
+}
