8e102f00ba1462d2bca3e6fc5254bea01121c6d7
Aaron <aaron.sadler@activeis.net>
2020-05-28 12:04:33 +0100

    Added logging

--- ActiveIS.UmbracoForms.CustomEmail/Helpers/HandleSmtp.cs
+++ ActiveIS.UmbracoForms.CustomEmail/Helpers/HandleSmtp.cs
@@ -1,11 +1,19 @@
-﻿using System;
+using System;
 using System.Net.Mail;
+using System.Runtime.InteropServices;
+using Umbraco.Core.Logging;
 
 namespace ActiveIS.UmbracoForms.CustomEmail.Helpers
 {
-    internal static class HandleSmtp
+    internal class HandleSmtp
     {
-        internal static void SendEmail(string emailBody, string toEmail, string fromEmail, string fromName, string subject)
+        private readonly ILogger _logger;
+
+        private HandleSmtp(ILogger logger)
+        {
+            _logger = logger;
+        }
+        internal void SendEmail(string emailBody, string toEmail, string fromEmail, string fromName, string subject)
         {
             MailMessage message = new MailMessage
             {
