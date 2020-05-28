7c7d9ef1f4849cdeda8b1b97161d93a6b128556c
Aaron <aaron.sadler@activeis.net>
2020-05-28 11:51:25 +0100

    Moved smtp to helper

---
+++ ActiveIS.UmbracoForms.CustomEmail/Helpers/HandleSmtp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace ActiveIS.UmbracoForms.CustomEmail.Helpers
+{
+    internal static class HandleSmtp
+    {
+        internal static void SendEmail(string emailBody, string toEmail, string fromEmail, string fromName, string subject)
+        {
+            MailMessage message = new MailMessage
+            {
+                From = new MailAddress(fromEmail, fromName),
+                Subject = subject,
+                Body = emailBody,
+                IsBodyHtml = true,
+            };
+
+            message.To.Add(toEmail);
+
+
+            SmtpClient client = new SmtpClient();
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
