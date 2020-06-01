006003c6b954e2b43b851a35bbd734c5731ba368
Aaron <aaron.sadler@activeis.net>
2020-06-01 20:35:17 +0100

    Return file path

---
+++ ActiveIS.UmbracoForms.CustomEmail/Controllers/ActiveISCustomEmailController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using Umbraco.Web.Editors;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace ActiveIS.UmbracoForms.CustomEmail.Controllers
+{
+    [PluginController("ActiveISCustomEmail")]
+    public class PickerController : UmbracoAuthorizedJsonController
+    {
+        public object GetVirtualPathForEmailTemplate(string encodedPath)
+        {
+            return new
+            {
+                path = ("Forms/CustomEmails/" + HttpUtility.UrlDecode(encodedPath))
+            };
+        }
+    }
+}
