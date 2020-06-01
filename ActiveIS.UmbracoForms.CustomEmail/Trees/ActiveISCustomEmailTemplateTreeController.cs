eee39ef3afc75762953ddcff8ead26c560ef6098
Aaron <aaron.sadler@activeis.net>
2020-06-01 20:34:56 +0100

    Return a list of all html and cshtml files in the CustomEmails folder

---
+++ ActiveIS.UmbracoForms.CustomEmail/Trees/ActiveISCustomEmailTemplateTreeController.cs
@@ -0,0 +1,41 @@
+﻿using Umbraco.Core.IO;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.Trees;
+
+namespace ActiveIS.UmbracoForms.CustomEmail.Trees
+{
+    [Tree("forms", "activeisCustomEmailTemplates", IsSingleNodeTree = true, TreeTitle = "Custom Email Templates", TreeUse = TreeUse.Dialog)]
+    [PluginController("ActiveISCustomEmail")]
+    public class ActiveISCustomEmailTemplateTreeController : FileSystemTreeController
+    {
+        private static readonly string[] ExtensionsStatic = new string[2]
+        {
+            "cshtml",
+            "html"
+        };
+
+        protected override IFileSystem FileSystem
+        {
+            get
+            {
+                return (IFileSystem)new PhysicalFileSystem("~/Views/Partials/Forms/CustomEmails");
+            }
+        }
+
+        protected override string[] Extensions
+        {
+            get
+            {
+                return ActiveISCustomEmailTemplateTreeController.ExtensionsStatic;
+            }
+        }
+
+        protected override string FileIcon
+        {
+            get
+            {
+                return "icon-article";
+            }
+        }
+    }
+}
